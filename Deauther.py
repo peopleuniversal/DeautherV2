924c99399d988dc7da91f1c99715776a28ec94b9
People Universal <135725113+peopleuniversal@users.noreply.github.com>
2024-09-15 11:00:24 +0700

    Update Deauther.py

--- Deauther.py
+++ Deauther.py
@@ -2,6 +2,14 @@
 
 import os, sys, time
 
+green   =   "\033[1;92m"
+white   =   "\033[1;97m"
+ash     =   "\033[1;90m"
+yellow  =   "\033[1;93m"
+purple  =   "\033[1;95m"
+red     =   "\033[1;91m"
+blue    =   "\033[1;96m"
+
 def menu():
     time.sleep(3)
     os.system("clear")
