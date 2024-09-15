4c3ede7281a3ebc3b14a2207792e393ba3d1061b
People Universal <135725113+peopleuniversal@users.noreply.github.com>
2024-09-16 06:51:56 +0700

    Update CheckMac.py

--- CheckMac.py
+++ CheckMac.py
@@ -2,16 +2,25 @@
 
 import os, sys, time
 
+# Color Text
+green   =   "\033[1;92m"
+white   =   "\033[1;97m"
+ash     =   "\033[1;90m"
+yellow  =   "\033[1;93m"
+purple  =   "\033[1;95m"
+red     =   "\033[1;91m"
+blue    =   "\033[1;96m"
+
 def menu():
     os.system("clear")
     time.sleep(3)
+    print(f"{red}***************************")
+    print(f"{white}* [ Author : Deauther   ] *")
+    print(f"{red}* [ Wi-fi Address Check ] *")
+    print(f"{white}* [1] Mac Check           *")
+    print(f"{red}* [0] Exit                *")
     print("***************************")
-    print("* [ Author : Deauther   ] *")
-    print("* [ Wi-fi Address Check ] *")
-    print("* [1] Mac Check           *")
-    print("* [0] Exit                *")
-    print("***************************")
-    tools = input("""Input Number > """)
+    tools = input(f"""{white}Input Number: """)
 
     if tools="1":
        os.system("clear")
@@ -22,19 +31,14 @@ def menu():
        os.system("airmon-ng start wlan0")
        time.sleep(3)
        os.system("clear")
-       os.system("airmon-ng check kill")
-       time.sleep(3)
-       os.system("clear")
        os.system("airodump-ng start wlan0")
-       print("Copy Mac Wi-fi And Paste In nano Deauthertest.py")
-       print("Cancel Command CTRL + C")
+       print("""Copy Mac Wi-fi And Paste In nano Deauthertest.py, Cancel Command CTRL + C.""")
 
     elif tools="0":
        os.system("clear")
-       print("Byee...!!!")
+       print("Byee...See You Again.")
        time.sleep(3)
        os.system("clear")
-       time.sleep(2)
        sys.exit()
 
     else:
@@ -42,5 +46,5 @@ def menu():
        print("Pls Input Code Valid...!!!")
        time.sleep(3)
        os.system("python CheckMac.py")
-       
+
 menu()
